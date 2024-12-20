322f5afc5ba9e26d72c6fad42a07da066e808d68
Nitesh Prajapati <niteshkmrprajapati298@gmail.com>
2024-12-20 19:21:55 +0530

    validation - profile errors

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -14,29 +14,26 @@ profileRouter.get("/profile/view",userAuth, async (req, res) => {
         res.status(401).send("Invalid or expired token" , error.message);
     }
 });
-profileRouter.patch("/profile/edit",userAuth, async (req,res) => {
 
-       
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     try {
-        if(!validateEditProfileData(req)){
-            throw new Error("Profile Edit Failed");
-
-        }
-        if(!validateEditFieldValues(req)){
-            throw new Error("Invalid Edit");
-        }
-        const loggedInUser = req.user;
-        Object.keys(req.body).forEach((key)=>(loggedInUser[key] = req.body[key]))
-        await loggedInUser.save();
-
-        res.json({
-            message:`${loggedInUser.firstName} , your profile updated successfully`,
-            data: loggedInUser,
-        });
-
-    } catch (error) {
-        res.status(400).send("ERROR: " + error.message)
-    };
-});
+      if (!validateEditProfileData(req)) {
+        throw new Error("Invalid Edit Request");
+      }
+  
+      const loggedInUser = req.user;
+  
+      Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+  
+      await loggedInUser.save();
+  
+      res.json({
+        message: `${loggedInUser.firstName}, your profile updated successfuly`,
+        data: loggedInUser,
+      });
+    } catch (err) {
+      res.status(400).send("ERROR : " + err.message);
+    }
+  });
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,81 +2,35 @@ const validator = require("validator");
 
 const validateSignUpData = (req) => {
   const { firstName, lastName, emailId, password } = req.body;
-
   if (!firstName || !lastName) {
-    throw new Error("First name or last name is not valid.");
+    throw new Error("Name is not valid!");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid!");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter a strong Password!");
   }
-
-  if (firstName.length < 4 || firstName.length > 50) {
-    throw new Error("First name should be between 4 to 50 characters.");
-  }
-  
-  if (!validator.isEmail(emailId)) {
-    throw new Error("Email address is not valid.");
-  }
-
-  if (!validator.isStrongPassword(password)) {
-    throw new Error("Please enter a strong password.");
-  }
-
-  // If everything is valid, you can return true or handle accordingly
-  return true;
 };
 
-const validateEditProfileData = (req) =>{
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoUrl",
+    "gender",
+    "age",
+    "about",
+    "skills",
+  ];
 
-  const allowedEditFields = ["firstName","lastName" ,"age","photoUrl","about","skills"];
-const isEditAllowed =  Object.keys(req.body).every(field=>allowedEditFields.includes(field));
-return isEditAllowed;
-  
-}
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
 
-const validateEditFieldValues = (req) => {
-  const { firstName, lastName, age, photoUrl, about, skills } = req.body;
-
-  // Check if firstName and lastName are present
-  if (!firstName || !lastName) {
-    throw new Error("First name or last name is not valid.");
-  }
-
-  // Validate firstName length
-  if (firstName.length < 4 || firstName.length > 50) {
-    throw new Error("First name should be between 4 to 50 characters.");
-  }
-
-  // Validate about length (in words)
-  const aboutWordCount = about.trim().split(/\s+/).length;
-  if (aboutWordCount > 30) {
-    throw new Error("About length can't be more than 30 words.");
-  }
-
-  // Validate age
-  if (age < 18) {
-    throw new Error("Age must be at least 18 years old.");
-  }
-  if (age > 100) {
-    throw new Error("Age must be correct and below 100 years.");
-  }
-
-  // Validate skills length
-  if (skills.length > 10) {
-    throw new Error("Skills can't be more than 10.");
-  }
-
-  // Validate photoUrl if present
-  if (photoUrl) {
-    const urlPattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
-    if (!urlPattern.test(photoUrl)) {
-      throw new Error("Photo URL is not valid.");
-    }
-  }
-  return true;
+  return isEditAllowed;
 };
-
-
 
 module.exports = {
   validateSignUpData,
   validateEditProfileData,
-  validateEditFieldValues,
 };
